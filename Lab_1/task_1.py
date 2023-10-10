fd2e69daf76d0289c02a728f0d32aa4662f7ca34
EmptyInsid <109950981+EmptyInsid@users.noreply.github.com>
2023-10-11 01:24:13 +0300

    Lab_1_first_version

---
+++ Lab_1/task_1.py
@@ -0,0 +1,12 @@
+#задание1: Замена пропущенного элемента средним арифметическим
+
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+wrong_num = 4
+sum_before = sum(numbers[:wrong_num])
+sum_after = sum(numbers[wrong_num+1:])
+count_of_num = len(numbers)
+
+numbers[wrong_num] = (sum_after + sum_before) / count_of_num
+
+print("Измененный список:", numbers)
